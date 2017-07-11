a93052feedc27fc4bd5d5353a8370b4a4dfd115e
Adam Urban <adam.karol.urban@gmail.com>
2017-07-11 11:04:25 +0200

    Added basic api service

--- front/common/index.js
+++ front/common/index.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
 import './common.scss'
 
+import ApiService from './api.service';
+
 export default angular.module('app.common', [])
+  .service('apiService', ApiService)
   .name;
